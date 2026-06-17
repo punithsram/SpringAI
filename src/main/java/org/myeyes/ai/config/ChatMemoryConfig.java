70fe454714f179136e6a8519f4cba124bc3684c4
punith <punithsramgowda26@gmail.com>
2026-06-17 23:16:03 +0530

    added chat options and also changed mistral model

--- src/main/java/org/myeyes/ai/config/ChatMemoryConfig.java
+++ src/main/java/org/myeyes/ai/config/ChatMemoryConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatMemoryConfig {
 
-
     @Bean
     ChatMemory chatMemory() {
         return MessageWindowChatMemory.builder().build();
